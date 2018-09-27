e39dd7cc1f0042e7c77edb2d7510dcd211b5e0b1
Georgi Gerganov <ggerganov@gmail.com>
2018-09-27 18:39:13 +0300

    [record] Better std output

--- record.cpp
+++ record.cpp
@@ -3,12 +3,14 @@
  *  \author Georgi Gerganov
  */
 
+#include "constants.h"
 #include "audio_logger.h"
 
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
 
+#include <map>
 #include <cstdio>
 #include <chrono>
 #include <thread>
@@ -29,6 +31,8 @@ int main(int argc, const char ** argv) {
 
     int keyPressed = -1;
     int bufferSize_frames = 2*AudioLogger::getBufferSize_frames(kSampleRate, kBufferSize_s) - 1;
+    std::map<int, int> nTimes;
+    printf("Recording %d frames per key press\n", bufferSize_frames);
 
     std::ofstream fout(argv[1], std::ios::binary);
     fout.write((char *)(&bufferSize_frames), sizeof(bufferSize_frames));
@@ -36,16 +40,18 @@ int main(int argc, const char ** argv) {
     AudioLogger audioLogger;
     AudioLogger::Callback cbAudio = [&](const auto & frames) {
         tEnd = std::chrono::high_resolution_clock::now();
-        printf("t = %d us\n", (int) std::chrono::duration_cast<std::chrono::microseconds>(tEnd - tStart).count());
 
-        printf("Record callback with %d frames\n", (int) frames.size());
         fout.write((char *)(&keyPressed), sizeof(keyPressed));
         for (const auto & frame : frames) {
             totalSize_bytes += sizeof(frame[0])*frame.size();
             fout.write((char *)(frame.data()), sizeof(frame[0])*frame.size());
             fout.flush();
         }
-        printf("Total data saved: %g MB\n", ((float)(totalSize_bytes)/1024.0f/1024.0f));
+        ++nTimes[keyPressed];
+
+        printf("Last recorded key - %3d '%s'. Total times recorded so far - %3d. Total data saved: %g MB\n",
+               keyPressed, kKeyText.at(keyPressed), nTimes[keyPressed], ((float)(totalSize_bytes)/1024.0f/1024.0f));
+
         keyPressed = -1;
     };
 
