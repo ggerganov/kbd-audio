3c4268f6bdf48c6b802f86c8181483e7ee8ae84a
Georgi Gerganov <ggerganov@gmail.com>
2018-12-05 21:14:05 +0200

    Remove old compile script

--- compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++14 -o record record.cpp key_logger.cpp audio_logger.cpp -framework CoreFoundation -framework CoreGraphics -framework SDL2
-g++ -std=c++14 -o play play.cpp -framework CoreFoundation -framework CoreGraphics -framework SDL2
