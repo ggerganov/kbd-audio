bdfefd1c609a9c1632678164d366cf89eb45a4d6
CheKecha19 <61182912+CheKecha19@users.noreply.github.com>
2021-04-14 21:26:03 +0300

    Update constants.h

--- constants.h
+++ constants.h
@@ -45,6 +45,7 @@ static constexpr int64_t kKeyDuration_samples = 0.005f*kSampleRate;
 
 static constexpr float kFreqCutoff_Hz = 1000.0f;
 
+char a = "ф";
 static std::map<char, std::vector<char>> kNearbyKeys = {
     { 'a', { 'a', 'q', 'w', 's', 'z', 'x',                               } },
     { 'b', { 'b', 'f', 'g', 'h', 'v', 'n',                               } },
