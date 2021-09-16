97ee306b69943b6a319dcd4703404366fc2861ba
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2021-09-16 13:11:16 +0300

    Refactor code

--- audio_logger.h
+++ audio_logger.h
@@ -14,59 +14,66 @@
 #include <functional>
 
 class AudioLogger {
-    public:
-        using Sample = TSampleF;
+public:
+    using Sample = TSampleF;
 
-        using Frame = std::array<Sample, kSamplesPerFrame>;
-        using Record = std::vector<Frame>;
-        using Callback = std::function<void(const Record & frames)>;
+    using Frame = std::array<Sample, kSamplesPerFrame>;
+    using Record = std::vector<Frame>;
+    using Callback = std::function<void(const Record &frames)>;
 
-        struct Parameters {
-            Callback callback;
+    struct Parameters {
+        Callback callback;
 
-            int32_t captureId = -1;
-            int32_t nChannels = -1;
+        int32_t captureId = -1;
+        int32_t nChannels = -1;
 
-            int64_t sampleRate = -1;
+        int64_t sampleRate = -1;
 
-            // Sample Type
+        // Sample Type
 
-            // todo : support for other sample types
-            enum ESampleType {
-                F32SYS,
-            };
-
-            ESampleType sampleType = F32SYS;
-
-            // Filter
-
-            enum EFilter {
-                None = 0,
-                FirstOrderHighPass,
-                SecondOrderButterworthHighPass,
-            };
-
-            EFilter filter = FirstOrderHighPass;
-
-            float freqCutoff_Hz = -1.0f;
+        // todo : support for other sample types
+        enum ESampleType {
+            F32SYS,
         };
 
-        AudioLogger();
-        ~AudioLogger();
+        ESampleType sampleType = F32SYS;
 
-        bool install(Parameters && parameters);
-        bool terminate();
-        bool addFrame(const Sample * stream);
-        bool record(float bufferSize_s, int32_t nPrevFrames);
+        // Filter
 
-        bool pause();
-        bool resume();
+        enum EFilter {
+            None = 0,
+            FirstOrderHighPass,
+            SecondOrderButterworthHighPass,
+        };
 
-        bool isValidBufferSize(float bufferSize_s) const;
+        EFilter filter = FirstOrderHighPass;
 
-    private:
-        struct Data;
-        std::unique_ptr<Data> data_;
-        Data & getData() { return *data_; }
-        const Data & getData() const { return *data_; }
+        float freqCutoff_Hz = -1.0f;
+    };
+
+    AudioLogger();
+
+    ~AudioLogger();
+
+    bool install(Parameters &&parameters);
+
+    bool terminate();
+
+    bool addFrame(const Sample *stream);
+
+    bool record(float bufferSize_s, int32_t nPrevFrames);
+
+    bool pause();
+
+    bool resume();
+
+    bool isValidBufferSize(float bufferSize_s) const;
+
+private:
+    struct Data;
+    std::unique_ptr <Data> data_;
+
+    Data &getData() { return *data_; }
+
+    const Data &getData() const { return *data_; }
 };
